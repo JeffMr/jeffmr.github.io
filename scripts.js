2ddcb1c160e3fced346c5adc06cf89c45cb76207
Jefferson Miranda <contatejeff@gmail.com>
2022-05-07 16:59:25 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
+/*Fecha alerta de informação*/
+function alertcloseFunction()  {
+  var close = document.getElementsByClassName("closebtn");
+  var i;
+            
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function(){
+      var div = this.parentElement;
+      div.style.opacity = "0";
+      setTimeout(function(){ div.style.display = "none"; }, 600);
+    }
+  }
+}
+
 /* Quando clicar no (código) ou botão (copiar), abrirá o popup.*/
 function mypopupFunction() {
   var popup = document.getElementById("myPopup");
